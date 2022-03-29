d2dd0bd3cab5e90f80b0544e1beb391c46d4274c
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-03-29 21:27:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-contact = input("contact name:\n")
-number = input("contact's number:\n")
-email = input("Email: ").strip()
-username = email[:email.index("@")]
-print(f"Contact name: {contact}\nNumber: {number}\nUsername: {username}")
+add = input("add contact? Y/N\n").lower()
+
+while add == "y":
+  contact = input("contact name:\n")
+  number = input("contact's number:\n")
+  email = input("Email: ").strip()
+  username = email[:email.index("@")]
+  print(f"Contact name: {contact}\nNumber: {number}\nUsername: {username}")
+  add = input("add contact? Y/N\n").lower()
+  if add == "n":
+    break
