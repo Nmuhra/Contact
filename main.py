3d2de341d438eea8e23b681227fd01df762ae664
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-06-30 20:08:25 +0000

    everything

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-add = input("add contact? Y/N\n").lower()
+names = []
+phone_numbers = []
+contacts = input("how many contacts?\n")
+num = int(contacts)
 
-while add == "y":
-  contact = input("contact name:\n")
-  number = input("contact's number:\n")
-  email = input("Email: ").strip()
-  username = email[:email.index("@")]
-  print(f"Contact name: {contact}\nNumber: {number}\nUsername: {username}")
-  add = input("add contact? Y/N\n").lower()
-  if add == "n":
-    break
+
+for i in range(num):
+    name = input("Name: ")
+    phone_number = input("Phone Number: ") # for convert to int => int(input("Phone Number: "))
+
+    names.append(name)
+    phone_numbers.append(phone_number)
+
+print("\nName\t\t\tPhone Number\n")
+
+for i in range(num):
+    print("{}\t\t\t{}".format(names[i], phone_numbers[i]))
+
+search_term = input("\nEnter search term: ")
+
+print("Search result:")
+
+if search_term in names:
+    index = names.index(search_term)
+    phone_number = phone_numbers[index]
+    print("Name: {}, Phone Number: {}".format(search_term, phone_number))
+
+else:
+    print("Name Not Found")
